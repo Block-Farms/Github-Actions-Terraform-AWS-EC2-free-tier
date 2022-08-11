3c596faab203f5255f2373b4b367896c52460099
mgladson <99776898+mgladson@users.noreply.github.com>
2022-08-11 00:02:10 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
-module "aws_instance" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#instance_type
+resource "aws_instance" "server" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-    source = "module/ecs"
+  tags = {
+    Name = var.tags_name
+  }
 
-# https://www.terraform.io/language/modules/sources#generic-git-repository
-    # source = "git::https://github.com/mgladson/Terraform-Modules-Chainlink.git//modules/aws/ec2-free-instance"
-
-    # EC2 instance variables
-    ami                           = var.ami
-    instance_type                 = var.instance_type
-    tags_name                     = var.tags_name
-    root_block_device_volume_size = var.root_block_device_volume_size 
+  root_block_device {
+    volume_size = var.root_block_device_volume_size  
+  }
 
 }
